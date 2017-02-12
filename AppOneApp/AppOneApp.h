910ccea022fd2dd5a7051ad2010de90a048c148a
Jonathan Unsworth <jonathan@bubbleworks.co.za>
2017-02-12 15:21:30 +0200

    Initial commit...

---
+++ AppOneApp/AppOneApp.h
@@ -0,0 +1,19 @@
+//
+//  AppOneApp.h
+//  AppOneApp
+//
+//  Created by Jonathan Unsworth on 2017/02/12.
+//  Copyright © 2017 Jonathan Unsworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppOneApp.
+FOUNDATION_EXPORT double AppOneAppVersionNumber;
+
+//! Project version string for AppOneApp.
+FOUNDATION_EXPORT const unsigned char AppOneAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppOneApp/PublicHeader.h>
+
+
